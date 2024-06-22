7da79af11a4634e47a94706bdb73481345077a31
mohammad <mohammadhoseinajorloo76@gmail.com>
2024-06-22 12:09:36 +0330

    feat(test): test main
    add first test class in project

---
+++ test/test_main.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class TestMain(unittest.TestCase):
+
+    def test_set_headers(self):
+        self.assertIsNone(None, "should return None")
+
+
+if __name__ == "__main__":
+    unittest.main()
