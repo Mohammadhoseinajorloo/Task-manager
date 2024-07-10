52375d479afe933da96872e48c9a93d7bf51bcba
mohammad <mohammadhoseinajorloo76@gmail.com>
2024-07-10 18:22:04 +0330

    feat(cache): redis
    add redis cache in the project

--- backend/cache/connection.py
+++ backend/cache/connection.py
@@ -1,1 +1,14 @@
+from backend.core.config import settings
+from redis import Redis
 
+
+def get_redis_connection(**kwargs) -> Redis:
+    if "decode_responses" not in kwargs:
+        kwargs["decode_responses"] = True
+
+    # If someone passed in a 'url' parameter, or specified a REDIS_OM_URL
+    # environment variable, we'll create the Redis client from the URL.
+    url = kwargs.pop("url", settings.CACHE_URL)
+    if url:
+        return Redis.from_url(url, **kwargs)
+    return Redis(**kwargs)
